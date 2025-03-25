e113679b45e1f003fcce2d160816534417902637
arupanda-hub <arabinda_panda@hotmail.com>
2025-03-25 00:27:58 -0400

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@
 
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 helpful_links = [
@@ -57,7 +56,10 @@ st.write("The name of your Smoothie will be:", name_on_order)
 
 
 # Display the Fruit Options List from the database table in Your Streamlit in Snowflake (SiS) App . 
-session = get_active_session()
+# session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 # my_dataframe = session.table("smoothies.public.fruit_options")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
